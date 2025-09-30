21c04cfd339d7e9cefbfb02084a5826c68806647
ScotDex <themadlyscientific@gmail.com>
2025-09-30 02:40:23 +0100

    oops

--- bot_service.go
+++ bot_service.go
@@ -217,7 +217,7 @@ func (s *Service) interactionCreate(sess *discordgo.Session, i *discordgo.Intera
 			}
 
 			embed = &discordgo.MessageEmbed{
-				Author:      &discordgo.MessageEmbedAuthor{Name: "ShortCircuit Route Planner", IconURL: "logo.png"},
+				Author:      &discordgo.MessageEmbedAuthor{Name: "ShortCircuit Route Planner", IconURL: "https://images.evetech.net/corporations/98330748/logo?size=64"},
 				Title:       "Route Calculated",
 				Color:       embedColor,
 				Timestamp:   time.Now().Format(time.RFC3339),
